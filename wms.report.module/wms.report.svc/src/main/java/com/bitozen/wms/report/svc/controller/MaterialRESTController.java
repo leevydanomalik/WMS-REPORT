514871ff20bb8f3f8b26ac0dc2b5c73e5ba44644
Izzy Khalam <IZZY KHALAM@DESKTOP-1985BOU>
2020-02-10 16:58:59 +0700

    Material service report

---
+++ wms.report.module/wms.report.svc/src/main/java/com/bitozen/wms/report/svc/controller/MaterialRESTController.java
@@ -0,0 +1,53 @@
+package com.bitozen.wms.report.svc.controller;
+
+import com.bitozen.wms.report.common.exception.GenericException;
+import com.bitozen.wms.report.common.type.FileExtention;
+import com.bitozen.wms.report.svc.ReportService;
+import java.io.ByteArrayOutputStream;
+import java.util.HashMap;
+import java.util.Map;
+import org.apache.commons.lang3.Validate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ *
+ * @author IZZY KHALAM
+ */
+@RestController
+@RequestMapping("report")
+public class MaterialRESTController {
+    
+     private final String MATERIAL_CSV = "Material.jasper";
+     
+     
+      @Autowired 
+    ReportService reportService;
+    
+    @ResponseBody
+    @RequestMapping(value = "data.material.csv/", method = RequestMethod.GET,
+            produces = "text/csv")
+    public byte[] generatePoMonitorReportCSV(@RequestParam(value = "reportFormat", required = false) FileExtention reportFormat ) throws GenericException {
+        Map<String, Object> params = new HashMap<>();
+        ByteArrayOutputStream os;
+        reportService.setRptResourcePrefix("/report/");
+        os = (ByteArrayOutputStream) reportService.showReportJdbcDataSourceExportToPdfTxtCsvXls(FileExtention.CSV, MATERIAL_CSV,params);
+
+        /*validate object*/
+        Validate.notNull(os);
+
+        /*get the byte*/
+        byte[] datastream = os.toByteArray();
+
+        /*validate object*/
+        Validate.notNull(datastream);
+
+        return datastream;
+    }
+    
+}
