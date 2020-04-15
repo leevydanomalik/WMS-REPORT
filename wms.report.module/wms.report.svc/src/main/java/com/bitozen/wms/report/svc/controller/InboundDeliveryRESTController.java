2e394d7a0b8775d7e702f8f15d50602438d4f8fe
toinktoat <toat.hidayatulloh@gmail.com>
2020-04-15 11:00:20 +0700

    Service download csv inbound delivery

---
+++ wms.report.module/wms.report.svc/src/main/java/com/bitozen/wms/report/svc/controller/InboundDeliveryRESTController.java
@@ -0,0 +1,52 @@
+package com.bitozen.wms.report.svc.controller;
+
+import com.bitozen.wms.report.common.exception.GenericException;
+import com.bitozen.wms.report.common.type.FileExtention;
+import com.bitozen.wms.report.svc.ReportService;
+import java.io.ByteArrayOutputStream;
+import java.util.HashMap;
+import java.util.Map;
+import org.apache.commons.lang3.Validate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ *
+ * @author ASUS
+ */
+@RestController
+@RequestMapping("report")
+public class InboundDeliveryRESTController {
+ 
+    private final String INBOUND_DELIVERY_CSV = "Inbound_delivery.jasper";
+     
+    @Autowired 
+    ReportService reportService;
+    
+    @ResponseBody
+    @RequestMapping(value = "data.inbound.delivery.csv/", method = RequestMethod.GET,
+            produces = "text/csv")
+    public byte[] generateInboundDeliveryReportCSV(@RequestParam(value = "reportFormat", required = false) FileExtention reportFormat ) throws GenericException {
+        Map<String, Object> params = new HashMap<>();
+        ByteArrayOutputStream os;
+        reportService.setRptResourcePrefix("/report/");
+        os = (ByteArrayOutputStream) reportService.showReportJdbcDataSourceExportToPdfTxtCsvXls(FileExtention.CSV, INBOUND_DELIVERY_CSV,params);
+
+        /*validate object*/
+        Validate.notNull(os);
+
+        /*get the byte*/
+        byte[] datastream = os.toByteArray();
+
+        /*validate object*/
+        Validate.notNull(datastream);
+
+        return datastream;
+    }
+    
+}
