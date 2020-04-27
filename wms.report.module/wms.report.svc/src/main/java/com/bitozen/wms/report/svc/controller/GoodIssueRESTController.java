0d68e9ec667546c67b6859a0bdc26358d1724c3b
Izzy Khalam <IZZY KHALAM@DESKTOP-1985BOU>
2020-04-27 12:57:24 +0700

    good issue report csv

---
+++ wms.report.module/wms.report.svc/src/main/java/com/bitozen/wms/report/svc/controller/GoodIssueRESTController.java
@@ -0,0 +1,52 @@
+package com.bitozen.wms.report.svc.controller;
+
+import com.bitozen.wms.report.common.exception.GenericException;
+import com.bitozen.wms.report.common.type.FileExtention;
+import com.bitozen.wms.report.svc.ReportService;
+import java.io.ByteArrayOutputStream;
+import java.util.HashMap;
+import java.util.Map;
+import org.apache.commons.lang3.Validate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ *
+ * @author IZZY KHALAM
+ */
+@RestController
+@RequestMapping("report")
+public class GoodIssueRESTController {
+    
+     private final String GOOD_ISSUE_CSV = "Good_issue.jasper";
+     
+    @Autowired 
+    ReportService reportService;
+    
+    @ResponseBody
+    @RequestMapping(value = "data.good.issue.csv/", method = RequestMethod.GET,
+            produces = "text/csv")
+    public byte[] generateGoodIssueReportCSV(@RequestParam(value = "reportFormat", required = false) FileExtention reportFormat ) throws GenericException {
+        Map<String, Object> params = new HashMap<>();
+        ByteArrayOutputStream os;
+        reportService.setRptResourcePrefix("/report/");
+        os = (ByteArrayOutputStream) reportService.showReportJdbcDataSourceExportToPdfTxtCsvXls(FileExtention.CSV, GOOD_ISSUE_CSV,params);
+
+        /*validate object*/
+        Validate.notNull(os);
+
+        /*get the byte*/
+        byte[] datastream = os.toByteArray();
+
+        /*validate object*/
+        Validate.notNull(datastream);
+
+        return datastream;
+    }
+    
+    
+}
