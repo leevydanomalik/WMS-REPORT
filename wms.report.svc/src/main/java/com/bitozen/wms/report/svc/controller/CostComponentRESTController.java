255d135350b736bdf215c422fa1ed88f6e9838dd
vulture <fahmi.hidayatullah12@gmail.com>
2020-05-14 14:39:06 +0700

    download csv cost component & coa storage location

---
+++ wms.report.svc/src/main/java/com/bitozen/wms/report/svc/controller/CostComponentRESTController.java
@@ -0,0 +1,51 @@
+package com.bitozen.wms.report.svc.controller;
+
+import com.bitozen.wms.report.common.exception.GenericException;
+import com.bitozen.wms.report.common.type.FileExtention;
+import com.bitozen.wms.report.svc.ReportService;
+import java.io.ByteArrayOutputStream;
+import java.util.HashMap;
+import java.util.Map;
+import org.apache.commons.lang3.Validate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ *
+ * @author vulture
+ */
+@RestController
+@RequestMapping("report")
+public class CostComponentRESTController {
+    
+    private final String COST_COMPONENT = "cost_component.jasper";
+
+    @Autowired
+    ReportService reportService;
+
+    @ResponseBody
+    @RequestMapping(value = "data.cost.component.csv/", method = RequestMethod.GET,
+            produces = "text/csv")
+    public byte[] generateCostComponentReportCSV(@RequestParam(value = "reportFormat", required = false) FileExtention reportFormat) throws GenericException {
+        Map<String, Object> params = new HashMap<>();
+        ByteArrayOutputStream os;
+        reportService.setRptResourcePrefix("/report/");
+        os = (ByteArrayOutputStream) reportService.showReportJdbcDataSourceExportToPdfTxtCsvXls(FileExtention.CSV, COST_COMPONENT, params);
+
+        /*validate object*/
+        Validate.notNull(os);
+
+        /*get the byte*/
+        byte[] datastream = os.toByteArray();
+
+        /*validate object*/
+        Validate.notNull(datastream);
+
+        return datastream;
+    }
+    
+}
