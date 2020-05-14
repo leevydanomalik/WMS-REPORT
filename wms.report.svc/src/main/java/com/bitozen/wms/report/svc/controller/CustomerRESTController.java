360a87ae96ae53b8d239fb64738fed9d3d162c3f
toinktoat <toat.hidayatulloh@gmail.com>
2020-05-14 17:22:22 +0700

    download csv asset , freigh pricing , customer

---
+++ wms.report.svc/src/main/java/com/bitozen/wms/report/svc/controller/CustomerRESTController.java
@@ -0,0 +1,51 @@
+package com.bitozen.wms.report.svc.controller;
+
+import com.bitozen.wms.report.common.exception.GenericException;
+import com.bitozen.wms.report.common.type.FileExtention;
+import com.bitozen.wms.report.svc.ReportService;
+import java.io.ByteArrayOutputStream;
+import java.util.HashMap;
+import java.util.Map;
+import org.apache.commons.lang3.Validate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ *
+ * @author ASUS
+ */
+@RestController
+@RequestMapping("report")
+public class CustomerRESTController {
+    
+    private final String CUSTOMER_CSV = "customer.jasper";
+
+    @Autowired
+    ReportService reportService;
+
+    @ResponseBody
+    @RequestMapping(value = "data.customer.csv/", method = RequestMethod.GET,
+            produces = "text/csv")
+    public byte[] generateCustomerReportCSV(@RequestParam(value = "reportFormat", required = false) FileExtention reportFormat) throws GenericException {
+        Map<String, Object> params = new HashMap<>();
+        ByteArrayOutputStream os;
+        reportService.setRptResourcePrefix("/report/");
+        os = (ByteArrayOutputStream) reportService.showReportJdbcDataSourceExportToPdfTxtCsvXls(FileExtention.CSV, CUSTOMER_CSV, params);
+
+        /*validate object*/
+        Validate.notNull(os);
+
+        /*get the byte*/
+        byte[] datastream = os.toByteArray();
+
+        /*validate object*/
+        Validate.notNull(datastream);
+
+        return datastream;
+    }
+    
+}
