7f6b09b7a483e5772c7e3280fe222e41fbfafdf8
Izzy Khalam <IZZY KHALAM@DESKTOP-1985BOU>
2020-12-22 14:54:53 +0700

    download baseprice item

--- wms.report.svc/src/main/java/com/bitozen/wms/report/svc/controller/MaterialBasePriceRESTController.java
+++ wms.report.svc/src/main/java/com/bitozen/wms/report/svc/controller/MaterialBasePriceRESTController.java
@@ -23,6 +23,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class MaterialBasePriceRESTController {
     
     private final String MATERIAL_BASE_PRICE_CSV = "material_base_price.jasper";
+    private final String MATERIAL_BASEPRICE_ITEM_CSV = "material_baseprice_item.jasper";
      
     @Autowired 
     ReportService reportService;
@@ -35,6 +36,27 @@ public class MaterialBasePriceRESTController {
         ByteArrayOutputStream os;
         reportService.setRptResourcePrefix("/report/");
         os = (ByteArrayOutputStream) reportService.showReportJdbcDataSourceExportToPdfTxtCsvXls(FileExtention.CSV, MATERIAL_BASE_PRICE_CSV,params);
+
+        /*validate object*/
+        Validate.notNull(os);
+
+        /*get the byte*/
+        byte[] datastream = os.toByteArray();
+
+        /*validate object*/
+        Validate.notNull(datastream);
+
+        return datastream;
+    }
+    
+    @ResponseBody
+    @RequestMapping(value = "data.material.base.price.item.csv", method = RequestMethod.GET,
+            produces = "text/csv")
+    public byte[] generateMaterialBasePriceItemReportCSV(@RequestParam(value = "reportFormat", required = false) FileExtention reportFormat ) throws GenericException {
+        Map<String, Object> params = new HashMap<>();
+        ByteArrayOutputStream os;
+        reportService.setRptResourcePrefix("/report/");
+        os = (ByteArrayOutputStream) reportService.showReportJdbcDataSourceExportToPdfTxtCsvXls(FileExtention.CSV, MATERIAL_BASEPRICE_ITEM_CSV,params);
 
         /*validate object*/
         Validate.notNull(os);
