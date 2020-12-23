7de24813aa3cef8fe0e4d2cde01a5907d9e00ed1
Izzy Khalam <IZZY KHALAM@DESKTOP-1985BOU>
2020-12-23 11:31:17 +0700

    report material by es

--- wms.report.svc/src/main/java/com/bitozen/wms/report/svc/controller/MaterialRESTController.java
+++ wms.report.svc/src/main/java/com/bitozen/wms/report/svc/controller/MaterialRESTController.java
@@ -6,6 +6,8 @@ import com.bitozen.wms.report.svc.ReportService;
 import java.io.ByteArrayOutputStream;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import org.apache.commons.lang3.Validate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -24,6 +26,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class MaterialRESTController {
     
      private final String MATERIAL_CSV = "Material.jasper";
+     private final String MATERIAL_NEW_CSV = "Material_new.jasper";
      
      
       @Autowired 
@@ -38,6 +41,32 @@ public class MaterialRESTController {
         reportService.setRptResourcePrefix("/report/");
         os = (ByteArrayOutputStream) reportService.showReportJdbcDataSourceExportToPdfTxtCsvXls(FileExtention.CSV, MATERIAL_CSV,params);
 
+        /*validate object*/
+        Validate.notNull(os);
+
+        /*get the byte*/
+        byte[] datastream = os.toByteArray();
+
+        /*validate object*/
+        Validate.notNull(datastream);
+
+        return datastream;
+    }
+    
+    @ResponseBody
+    @RequestMapping(value = "/data.material/{clientID}/{companyID}", method = RequestMethod.GET,
+            produces = "text/csv")
+    public byte[] generateMaterialByESReportCSV(@PathVariable("clientID") String clientID,@PathVariable("companyID") String companyID) {
+        Map<String, Object> params = new HashMap<>();
+        params.put("clientid", clientID);
+        params.put("companyid", companyID);
+        ByteArrayOutputStream os = new ByteArrayOutputStream();
+        reportService.setRptResourcePrefix("/report/");
+        try {
+            os = (ByteArrayOutputStream) reportService.showReportJdbcDataSourceExportToPdfTxtCsvXls(FileExtention.CSV, MATERIAL_NEW_CSV, params);
+        } catch (GenericException ex) {
+            Logger.getLogger(MaterialBasePriceRESTController.class.getName()).log(Level.SEVERE, null, ex);
+        }
         /*validate object*/
         Validate.notNull(os);
 
